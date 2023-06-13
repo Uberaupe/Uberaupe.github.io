a558d7e72e85c7aef933b4433dd66edb12cf835c
GabeMCM <gabrielmateuscm@gmail.com>
2023-06-13 13:01:47 -0300

    sass and login page

---
+++ js/index.js
@@ -0,0 +1,16 @@
+const user = document.querySelector("#user");
+const pass = document.querySelector("#password");
+const message = document.querySelector(".message");
+
+const data = {
+	user: "Uberaupe",
+	pass: "Uberaupe33343900",
+};
+
+const openPage = () => {
+	if (user.value === data.user && pass.value === data.pass) {
+		window.location.href = "http://Uberaupe.github.io/garantia.html";
+	} else {
+		message.innerHTML = "Nome do usuario ou senha incorreto. Tente novamente.";
+	}
+};
